10132e065912c733539095972667461bafff243b
accmanage <accmanage121@gmail.com>
2025-08-22 00:06:13 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
-// src/App.tsx
-import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "@/pages/login";
-
+import AdminDashboard from "@/components/AdminDashboard";
+import StaffDashboard from "@/components/StaffDashboard";
 
 function App() {
-  return <Login />;
+  return (
+    <Routes>
+      {/* Login page */}
+      <Route path="/login" element={<Login />} />
+
+      {/* Admin Dashboard */}
+      <Route path="/" element={<AdminDashboard />} />
+
+      {/* Staff Dashboard */}
+      <Route path="/staff" element={<StaffDashboard />} />
+
+      {/* Redirect any unknown route to login */}
+      <Route path="*" element={<Navigate to="/login" replace />} />
+    </Routes>
+  );
 }
 
 export default App;
