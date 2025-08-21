44741ce57c58c4b207620ef003035e10a549d310
accmanage <accmanage121@gmail.com>
2025-08-21 23:31:09 +0530

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { supabase } from "@/integrations/supabase/client";
+import { useNavigate } from "react-router-dom";
 
 import { Button } from "@/components/ui/button";
 
