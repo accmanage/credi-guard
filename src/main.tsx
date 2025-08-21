3e3a4248054ddd20435da57ddd8504b3c551a966
accmanage <accmanage121@gmail.com>
2025-08-22 00:00:06 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,13 @@
+// src/main.tsx
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-
-function ErrorBoundary({ children }: { children: React.ReactNode }) {
-  try {
-    return <>{children}</>;
-  } catch (e) {
-    console.error("Runtime error:", e);
-    return <div style={{ color: "red" }}>Error: {String(e)}</div>;
-  }
-}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ErrorBoundary>
+    <BrowserRouter>
       <App />
-    </ErrorBoundary>
+    </BrowserRouter>
   </React.StrictMode>
 );
